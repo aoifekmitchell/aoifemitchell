44b3799811791c10ffb91f95da20e21e7d53da94
AoifeM <mitcheao@tcd.ie>
2023-03-02 17:41:33 +0000

    update  netlify project files to Git

--- pages/Components/NavItem.jsx
+++ pages/Components/NavItem.jsx
@@ -2,11 +2,10 @@ import Link from "next/link";
 
 const NavItem = ({ text, href, active }) => {
   return (
-    <Link href={href}
+    <Link href={href ?? ""}
           className={`nav__item ${active ? "active" : ""}`}>
         {text}
     </Link>
   );
-};
-
+}
 export default NavItem;
